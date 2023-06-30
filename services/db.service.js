ee296c68da1be3c6709518e2c70ca6fe9962deb0
Lorenzo La Piana <109413745+Metaluppolo@users.noreply.github.com>
2023-06-30 23:43:34 +0200

    mysql connection is created just once

--- services/db.service.js
+++ services/db.service.js
@@ -1,14 +1,19 @@
 const config = require('../config');
 const mysql = require('mysql2/promise');
 
+let connection;
+async function connect() {
+    if (!connection)
+        connection = await mysql.createConnection(config.db);
+}
 
 // Connect to MySQL database and execute query
 async function query(sql, params) {
-    const connection = await mysql.createConnection(config.db);
+    await connect();
     const [results, ] = await connection.execute(sql, params);
-
     return results;
 }
+
 
 
 module.exports = { query }
